efa06f771db612fab11a4a70662074491c6ab166
Naved <naved1234@gmail.com>
2018-09-24 14:05:27 +0530

    code addition

---
+++ Data Structures & Algorithms/CodingNinjas/Trees/vector.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> v;
+  v.push_back(10);
+  v.push_back(20);
+  v.push_back(30);
+  cout << v[0] << endl;
+  cout << v[1] << endl;
+  cout << v.at(2) << endl;
+  return 0;
+}
